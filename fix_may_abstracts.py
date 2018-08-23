e2750cd798b9e8ffc9dae03c1256f3e4387f0b01
Andy Byers <andy.byers@openlibhums.org>
2018-08-23 15:07:26 +0100

    added managment command

---
+++ fix_may_abstracts.py
@@ -0,0 +1,47 @@
+import json
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from journal import models
+from submission import models as submission_models
+
+
+def find_article_in_dict(file_dict, article):
+    for obj in file_dict:
+        if obj['model'] == 'submission.article' and obj['pk'] == article.pk:
+            print('Object {pk} found - {date}'.format(pk=article.pk, date=article.date_published))
+
+
+class Command(BaseCommand):
+    """Fixes abstracts that were corrupted on the 16th of May, 2018"""
+
+    help = "Fixes abstracts that were corrupted on the 16th of May, 2018."
+
+    def add_arguments(self, parser):
+        """ Adds arguments to Django's management command-line parser.
+
+        :param parser: the parser to which the required arguments will be added
+        :return: None
+        """
+        parser.add_argument('file')
+
+    def handle(self, *args, **options):
+        file_path = options.get('file')
+
+        file = open(file_path, 'r')
+        file_dict = json.loads(file.read())
+
+        journal_codes = ['orbit', 'wwe', 'ddl']
+
+        journals = models.Journal.objects.filter(code__in=journal_codes)
+
+        print(journals)
+        date_affected = datetime(year=2018, month=5, day=15)
+
+        for journal in journals:
+            print('Running {journal}'.format(journal=journal.name))
+            articles = submission_models.Article.objects.filter(journal=journal, date_published__lte=date_affected)
+
+            for article in articles:
+                find_article_in_dict(file_dict, article)
